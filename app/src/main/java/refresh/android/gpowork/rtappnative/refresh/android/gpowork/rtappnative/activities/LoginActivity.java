6c6fe5b00138b75a4dcce31a4e34bcf2e4752840
gpowork <gpowork@gmail.com>
2017-05-14 13:21:22 -0500

    Added validation for login inputs.

--- app/src/main/java/refresh/android/gpowork/rtappnative/refresh/android/gpowork/rtappnative/activities/LoginActivity.java
+++ app/src/main/java/refresh/android/gpowork/rtappnative/refresh/android/gpowork/rtappnative/activities/LoginActivity.java
@@ -3,6 +3,8 @@ package refresh.android.gpowork.rtappnative.refresh.android.gpowork.rtappnative.
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import refresh.android.gpowork.rtappnative.R;
 
@@ -19,6 +21,15 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void runLoginProccess(View view) {
-        
+        EditText username = (EditText)findViewById(R.id.username);
+        EditText password = (EditText)findViewById(R.id.password);
+        if (password.getText() != null && password.getText().length() > 0
+                && username.getText() != null && username.getText().length() > 0) {
+
+        } else {
+            CharSequence text = "Data you put is not correct. Check it and press 'Login' button again.";
+            Toast toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG);
+            toast.show();
+        }
     }
 }
